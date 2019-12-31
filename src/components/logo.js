0113de16c9ba6c8c0ec7a3191565158ebe0d4d47
Reggie Peterson <reginald.peterson2@nih.gov>
2019-12-31 17:28:11 -0500

    quick merge conflict fix

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,10 +1,5 @@
 import React from "react"
 
 export default () => (
-<<<<<<< HEAD
-  <Link to="/" className="logo">
-  </Link>
-=======
   <div className="logo"> </div>
->>>>>>> b3d06f6fd773e4482a483e481b6fb019a314c772
 )
