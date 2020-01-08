df2ba229dd39afdefefd65a70f3f235361dd04aa
rpeterson <rpeterson@cbcits-mbp.cbc.corp>
2020-01-08 12:10:32 -0500

    add dashboard to sanity

---
+++ csccms/src/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+      {
+      name: 'netlify',
+      options: {
+        title: 'My Netlify deploys',
+        sites: [
+          {
+            title: 'City Soul Website',
+            apiId: 'fa98b025-2b74-43ae-8ad9-01ad9359c711',
+            buildHookId: '5e16075d5c36122a1d7cbe44',
+            name: 'nifty-albattani-357c81'
+          }
+        ]
+      }
+    }
+  ]
+}
